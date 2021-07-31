e9e91b6810c76b4a7f625998b83bde7a89d006fe
bob <muhfaisalll150402@gmail.com>
2021-07-31 18:42:40 +0700

    first commit

---
+++ belajarJs/script.js
@@ -0,0 +1,101 @@
+// apasih variable itu?
+// variable itu adalh penampung suatu data/nilai
+// apasih kegunaan nya?
+// console.log("")
+// let nama = "faisal";
+// nama = "icus ";
+// console.log(nama);
+// apa beda nya let ama const ini?
+// let nilainya bisa di ubah const gabisa(const gabakal bisa di tiban )
+// variable sifat nya bisa di panggil berulang ulang
+// tipe data
+// -number
+// -string
+// -boolean
+// apasih string itu ? string dinilai dengan dua tanda petik
+// bedanya string dan integer?
+// console.log("jum'at");
+// console.log(45564);
+// true;
+// false;
+// kapan boolean?
+// boolean untuk menetapkan logic
+// console.log(1 < 2);
+// apaaja sih operator perbandingan itu?
+// ==
+// apasih ==? bedanya == dan === apasih?
+// gunakan tergantung kebutuhan
+// == bandingin 2 nilai gk liat tipe data
+// === bandngin 2 nilat dengan bandingin tipe data nya (lebih ketat)
+// console.log(1 == "1"); //true
+// console.log(1 === "1"); //false
+// != tidak sama dengan gk liat tipe data
+// !== tidak sama dengan liat tipe data (lebih ketat)
+// console.log(5 != 1); true
+// > lebih dari
+// < kurang dari
+// >= lebih dari sama dengan
+// <= kurang dari sama dengan
+// console.log(5 <= 5); //true kenapa ?karena 5 lebih dari "SAMA DENGAN " 5
+// console.log(5 <= 5); //false karna 5 gk lebih dari 5 "tampa sama dengan"
+// apasih and itu and itu juga operator perbandingan yang bersimbol &&
+//komentar untuk catetan disini
+// console.log(true && true); //maka hasil nya false
+// console.log(true || false); //maka hasilnya true
+// console.log(!true); //dibaca tidak true apasih kebalikan yang bukan true  ?ya false
+// console.log(!false);
+// oprator matematika
+// +
+// let x = 10;
+// let z = 20;
+// let hasil = z + x; //pertamhan 20 + 10
+// console.log(hasil);30
+// let x = 10;
+// let z = 20;
+// let hasil = z - x; //pertamhan 20 - 10
+// console.log(hasil);10
+// let x = 10;
+// let z = 20;
+// let hasil = z / x; //pertamhan 20 / 10
+// console.log(hasil); //2
+//2 pangkat 4
+//2x2x2x2
+// let x = 10;
+// let z = 20;
+// let hasil = z ** x; //20x20x20x2020x20x20x20x20x20
+// console.log(hasil);
+// sifat pembagian ?
+// 1000 dibagi 1 gimana cara menemuin hasilnya 10000
+// modulus adalah sisa pembagian
+// console.log(5 % 3);
+// // 5-3  gabisa -3 karena mines jadihasil nya 2    // 2
+// console.log(6 % 3);
+// 6-3 -3 //0
+// itulah sifat sisa bagi
+
+// apasih incremenet itu? (++)
+// +1
+// let angka = 7;
+// angka++;
+// // dibalik layar angka nya diubah menjadi angka=angka+1
+// console.log(angka);
+// let angka = 7;
+// angka = angka - 1;
+// // dibalik layar angka nya diubah menjadi angka=angka-1
+// console.log(angka);
+// console.log(1 + 2 * 2);
+//1+2 *2  = 1+2 1+2
+// apasih perkondision itu?
+// let umur = 25;
+// if (umur > 17) {
+//   // apakah umur lebih dari 17 jika iya
+//   //   jalankan code ini
+//   console.log("kamu udah remja");
+// } else {
+//   // jika tidak jalankan code ini
+//   console.log("kamu masih bocil");
+// }
+/*
+
+
+*/
---
+++ belajarJs/scriptkedua.js
@@ -0,0 +1,76 @@
+/*
+variable,
+menamai /menampung suatu data
+*/
+
+/*
+!==
+*/
+
+// console.log(jeniskelaminlu !== jeniskelaminoranglain);
+// apakah cowo gak sama ama cewe ?yes
+/*
+if else 
+*/
+// if (jeniskelaminlu !== jeniskelaminoranglain) {
+//   console.log("hay kenalan dong");
+// } else {
+//   console.log("ajg lgbt");
+// }
+// let umur = 15;
+// if (umur < 17) {
+//   console.log("bocil lu anjig");
+// } else {
+//   console.log("lu tua blog");
+// }
+
+/*
+ditempat wahana dufan ada suatu permain yang bernamakan rolerCoster
+-syarat menaiki roler coster tersebut ialah umur "LEBIH DARI"  15 tahun
+-JIKA umur anak itu lebih dari 15 tampilkan kata "silakan masuk"
+-JIKA umur anak itu kurang dari 15 tampilkan kata "gabole masuk dek"
+*/
+// pertama kita buat variable bernama UMUR
+// const umur = prompt("silakan masukan umur anda");
+// // cek apakah umur lebih dari 15
+// if (umur > 15) {
+//   //jika iya
+//   alert("silakan masuk");
+// } else {
+//   // jika tidak
+//   alert("gabole masuk dek");
+// }
+/* gimana sih rumus menghitung umur dari tahun lahir kita?
+tahunini - tahunLahir lu
+2021 -2002
+
+*/
+// buat variable tahun lahir
+// let tahunLahir = 1900;
+// let tahunSaatIni = 2021;
+// if (tahunSaatIni - tahunLahir < 17) {
+//   console.log("wah masih muda");
+// } else if (tahunSaatIni - tahunLahir < 30) {
+//   console.log("wah kawin blog");
+// } else if (tahunSaatIni - tahunLahir < 40) {
+//   console.log("punya anak");
+// } else if (tahunSaatIni - tahunLahir < 50) {
+//   console.log("punya cucu");
+// } else if (tahunSaatIni - tahunLahir < 60) {
+//   console.log("cari bini muda");
+// } else if (tahunSaatIni - tahunLahir < 70) {
+//   console.log("pensiun");
+// } else {
+//   console.log("mati lu sana");
+// }
+
+//
+// */
+/*buat variable bernama umur
+let umur=
+if(umur<17){
+    tampilin apa
+}else{
+    tampilin apa
+}
+*/
